63c880298bccf1a25e4cb29a201a6e1a4760efc1
Julieta Agüero <julietaaguero021@gmail.com>
2023-10-25 11:50:18 -0300

    Se agregó el proyecto

---
+++ cr-pi-countries-main/server/src/models/Activity.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('Activity', {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      season: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      countryId: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    });
+  };
